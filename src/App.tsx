d262374de870d4d50d5857912714bd3b868923d0
srishti-maurya <srishti22maurya@gmail.com>
2022-11-12 23:45:04 +0530

    refactor: updated code

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { BarChart } from './components/BarChart';
+import { ScatterPlot } from './components/ScatterPlot';
+import { Data } from './types/type';
 
-function App() {
+const App: React.FC = (): JSX.Element => {
+  const [data, setData] = useState<Data[]>([]);
+
+  const getData = (): void => {
+    fetch('dataset.json', {
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((myJson) => setData(myJson));
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BarChart data={data} />
+      <ScatterPlot data={data} />
     </div>
   );
-}
+};
 
 export default App;
