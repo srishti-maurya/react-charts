149b2d2e7bf54536efadfeb18dc49051c5ad126e
srishti-maurya <srishti22maurya@gmail.com>
2022-11-12 23:44:48 +0530

    feat: added types

---
+++ src/types/type.ts
@@ -0,0 +1,21 @@
+export type Data = {
+  Alcohol: number;
+  Ash: number;
+  Magnesium: number;
+  Flavanoids: number;
+  Proanthocyanins: String;
+  Hue: number;
+  Unknown: number;
+  'Malic Acid': number;
+  'Alcalinity of ash': number;
+  'Total phenols': number;
+  'Nonflavanoid phenols': number;
+  'Color intensity': number;
+  'OD280/OD315 of diluted wines': number;
+};
+
+export type ChartProps = {
+  data: Data[];
+};
+
+export type Option = { xAxis: {}; yAxis: {}; series: any[]; tooltip: {} };
